d0b0f7aacc3d6a8abbd2abe588b10b924f5d2656
Ralf Hense <ralf.hense@1und1.de>
2019-01-04 19:53:09 +0100

    fix variables

--- 4_export_inference_graph.sh
+++ 4_export_inference_graph.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+#
+# hmm in model/ there is already an frozen_inference_graph.pb
+#
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 #
@@ -9,6 +12,7 @@ PIPELINE_CONFIG_PATH=${BASEDIR}/training_data/ssd_inception_v2_coco.config
 INPUT_TYPE=image_tensor
 
 TRAINED_CKPT_PREFIX=${BASEDIR}/model
+
 EXPORT_DIR=${BASEDIR}/exported_model
 
 
