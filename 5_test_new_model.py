9eb4faab1e844b717a3056ec164e5f7d2dcaa7be
Ralf Hense <ralf.hense@1und1.de>
2019-01-04 19:16:28 +0100

    fix import errors

--- 5_test_new_model.py
+++ 5_test_new_model.py
@@ -5,8 +5,8 @@ import os
 import tensorflow as tf
 import cv2
 
-from utils import label_map_util
-from utils import visualization_utils as vis_util
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
 from distutils.version import StrictVersion
 
 # module level variables ##############################################################################################
