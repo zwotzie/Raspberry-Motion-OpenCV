e1317c9b3e05a3c57dd911d774c86d3303e448bf
Ralf Hense <ralf.hense@1und1.de>
2019-01-06 11:00:35 +0100

    script if you want to start from scratch again for training

--- move_trained_model.sh
+++ move_trained_model.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
+#
+# if you want to clean up the models and start from scratch again
+#
+
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${BASEDIR}/train_configuration.rc
 
 
-
+# make backup of existing trained model and create fresh model directory
 mv ${MODEL_DIR} ${MODEL_DIR}-$(date --iso-8601)
-
 mkdir ${MODEL_DIR}
 
-cp ${PRETRAINED_MODEL_DIR}/* ${MODEL_DIR}
+cp ${PRETRAINED_MODEL_DIR}/* ${MODEL_DIR}/
+
+
+# make backup of exported trained model and create a fresh directory
+if find ${EXPORT_MODEL_DIR} -mindepth 1 | read; then
+    mv ${EXPORT_MODEL_DIR} ${EXPORT_MODEL_DIR}-$(date --iso-8601)
+    mkdir ${EXPORT_MODEL_DIR}
+fi
