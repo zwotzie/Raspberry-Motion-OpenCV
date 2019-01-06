0fc6dde9ecb27ed842eb27151dd95b23cf6afbc7
Ralf Hense <ralf.hense@1und1.de>
2019-01-06 15:16:48 +0100

    add PYTHONPATH for tensorflow model

--- raspberry/on_event_end.sh
+++ raspberry/on_event_end.sh
@@ -4,5 +4,7 @@ event_id=$1
 
 source ~/py3/bin/activate
 
+export PYTHONPATH=${PYTHONPATH}:/home/pi/models/research/:/home/pi/models/research/slim
+
 # run in background and with low niceness:
 nice -10 python on_event_end_tf.py ${event_id} &
