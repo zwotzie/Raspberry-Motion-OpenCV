1122e523c8549387cca320547429824c2563f865
Ralf Hense <rh@web.de>
2019-01-13 09:15:44 +0100

    call pythonscript with absolut path

--- raspberry/on_event_end.sh
+++ raspberry/on_event_end.sh
@@ -4,7 +4,10 @@ event_id=$1
 
 source ~/py3/bin/activate
 
+
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export PYTHONPATH=${PYTHONPATH}:/home/pi/models/research/:/home/pi/models/research/slim
 
 # run in background and with low niceness:
-nice -10 python on_event_end_tf.py ${event_id} &
+nice -10 python ${BASEDIR}/on_event_end_tf.py ${event_id} &
