74cbe7f6eb53faff3c623b9a6e1b4da8831a8daa
Ralf Hense <ralf.hense@1und1.de>
2019-02-05 21:45:27 +0100

    unicorn bind is appropriate only for localhost because of nginx proxy

--- raspberry/start_flask.sh
+++ raspberry/start_flask.sh
@@ -6,5 +6,5 @@ source /home/pi/py3/bin/activate
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${BASEDIR}
 
-exec gunicorn -b 0.0.0.0:5000 --workers=1 --access-logfile=${BASEDIR}/logs/access.log --error-logfile=${BASEDIR}/logs/error.log app:app
+exec gunicorn -b 127.0.0.1:5000 --workers=1 --access-logfile=${BASEDIR}/logs/access.log --error-logfile=${BASEDIR}/logs/error.log app:app
 # python app.py
