a46da4264bcc4164bdac3d85b75dbae5b880e99e
Ralf Hense <ralf.hense@1und1.de>
2019-02-05 21:39:40 +0100

    add missing = to gunicorn start

--- raspberry/start_flask.sh
+++ raspberry/start_flask.sh
@@ -6,5 +6,5 @@ source /home/pi/py3/bin/activate
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${BASEDIR}
 
-exec gunicorn -b 0.0.0.0:5000 --workers=1 --access-logfile ${BASEDIR}/access.log --error-logfile ${BASEDIR}/error.log app:app
+exec gunicorn -b 0.0.0.0:5000 --workers=1 --access-logfile=${BASEDIR}/access.log --error-logfile=${BASEDIR}/error.log app:app
 # python app.py
