0cd87aa6f1afd8a85c991d6df1c64f48b81a9a49
Ralf Hense <ralf.hense@1und1.de>
2019-06-25 20:40:20 +0200

    should never run with event_id = 0

--- raspberry/on_event_end.sh
+++ raspberry/on_event_end.sh
@@ -4,10 +4,11 @@ event_id=$1
 
 source ~/py3/bin/activate
 
-
-
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export PYTHONPATH=${PYTHONPATH}:/home/pi/models/research/:/home/pi/models/research/slim
 
 # run in background and with low niceness:
-nice -10 python ${BASEDIR}/on_event_end_tf.py ${event_id} &
+if [[ $1 > 0 ]]
+then
+    nice -10 python ${BASEDIR}/on_event_end_tf.py ${event_id} &
+fi
